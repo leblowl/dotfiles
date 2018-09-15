74291ae456fd7b83b22d2cf890b81618be97e7da
Lucas Leblow <leblowl@gmail.com>
2018-09-14 23:26:24 -0600

    update emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -50,7 +50,7 @@
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (company company-mode telephone-line telelphone-line cider lispyville lispy magit clojure-mode neotree projectile general which-key helm zenburn-theme evil use-package))))
+    (ivy magit company cider lispyville lispy clojure-mode evil helm-ag helm telephone-line neotree projectile general which-key zenburn-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -71,6 +71,19 @@
   :config
   (which-key-mode 1))
 
+;; Custom keybinding
+(defconst dft-prefix-key "SPC")
+
+(use-package general
+  :ensure t)
+
+;; Projectile
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode 1))
+
+;; Neotree
 (defun neotree-project-dir-toggle ()
   "Open NeoTree using the project root, using find-file-in-project,
 or the current buffer directory."
@@ -93,91 +106,6 @@ or the current buffer directory."
         (if file-name
             (neotree-find file-name))))))
 
-;; Custom keybinding
-(use-package general
-  :ensure t
-  :config
-
-  ;; Basic navigation
-  (general-define-key
-   :keymaps           '(normal visual insert emacs)
-   :prefix            "SPC"
-   :non-normal-prefix "M-SPC"
-
-   "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
-   "SPC" '(helm-M-x :which-key "M-x")
-
-   ;; Project
-   "pf"  '(helm-find-files :which-key "find files")
-   "pt"  '(neotree-project-dir-toggle :which-key "project tree")
-   "pp"  'projectile-switch-project
-
-   ;; Buffers
-   "bb"  '(helm-mini :which-key "helm mini")
-   "bd"  '(kill-current-buffer :whick-key "kill buffer")
-
-   ;; Window
-   "wl"  '(windmove-right :which-key "move right")
-   "wh"  '(windmove-left :which-key "move left")
-   "wk"  '(windmove-up :which-key "move up")
-   "wj"  '(windmove-down :which-key "move bottom")
-   "w/"  '(split-window-right :which-key "split window right")
-   "w-"  '(split-window-below :which-key "split window bottom")
-   "wd"  '(delete-window :which-key "kill window")
-   "wm"  '(delete-other-windows :which-key "delete other windows")
-
-   ;; Others
-   "at"  '(ansi-term :which-key "open terminal"))
-
-  ;; Global C-g override
-  (general-define-key
-   :keymaps 'key-translation-map "ESC" "C-g")
-
-  ;; Emacs Lisp
-  (general-define-key
-   :state   'normal
-   :keymaps 'emacs-lisp-mode-map
-   :prefix  ","
-   "eb" 'eval-buffer))
-
-;; Vim mode
-(use-package evil
-  :ensure t
-  :config
-  (setq evil-want-integration 1)
-  (evil-mode 1))
-
-;; Helm
-(use-package helm
-  :ensure t
-  :init
-  (setq helm-M-x-fuzzy-match t
-        helm-mode-fuzzy-match t
-        helm-buffers-fuzzy-matching t
-        helm-recentf-fuzzy-match t
-        helm-locate-fuzzy-match t
-        helm-semantic-fuzzy-match t
-        helm-imenu-fuzzy-match t
-        helm-completion-in-region-fuzzy-match t
-        helm-candidate-number-list 150
-        helm-split-window-in-side-p t
-        helm-move-to-line-cycle-in-source t
-        helm-echo-input-in-header-line t
-        helm-autoresize-max-height 0
-        helm-autoresize-min-height 20)
-
-  :config
-  (helm-mode 1))
-
-;; Projectile
-(use-package projectile
-  :ensure t
-  :init
-  (setq projectile-require-project-root nil)
-  :config
-  (projectile-mode 1))
-
-;; NeoTree
 (defun spacemacs/neotree-collapse ()
   "Collapse a neotree node."
   (interactive)
@@ -203,64 +131,120 @@ or the current buffer directory."
 (use-package neotree
   :ensure t
   :config
-  (progn
-    (setq
-     neo-theme 'nerd
-     neo-window-width 32
-     neo-create-file-auto-open t
-     neo-show-updir-line nil
-     neo-mode-line-type 'neotree
-     neo-smart-open t
-     neo-dont-be-alone t
-     neo-persist-show nil
-     neo-show-hidden-files t
-     neo-auto-indent-point t
-     neo-modern-sidebar t
-     neo-vc-integration nil))
+  (setq
+   neo-theme                 'nerd
+   neo-window-width          32
+   neo-create-file-auto-open t
+   neo-show-updir-line       nil
+   neo-mode-line-type        'neotree
+   neo-smart-open            t
+   neo-dont-be-alone         t
+   neo-persist-show          nil
+   neo-show-hidden-files     t
+   neo-auto-indent-point     t
+   neo-modern-sidebar        t
+   neo-vc-integration        nil)
 
   (general-define-key
    :states   '(normal emacs)
    :keymaps  'neotree-mode-map
-   "n"       'neotree-select-next-sibling-node
-   "p"       'neotree-select-previous-sibling-node
-   "h"       'spacemacs/neotree-collapse-or-up
-   "H"       'neotree-hidden-file-toggle
-   "y"       'neotree-copy-node
-   "c"       'neotree-create-node
+   "h"       'neotree-select-up-node
+   "l"       '(neotree-enter :which-key "enter")
+   "K"       'neotree-select-previous-sibling-node
+   "J"       'neotree-select-next-sibling-node
+   "c"       'neotree-copy-node
+   "C"       'neotree-create-node
    "R"       'neotree-change-root
    "r"       'neotree-rename-node
    "o"       'neotree-open-file-in-system-application
    "d"       'neotree-delete-node
    "RET"     'neotree-enter
-   "<tab>"   'neotree-enter
-   "l"       '(neotree-enter :which-key "enter")))
+   "<tab>"   'neotree-enter))
+
+;; Fancy mode line
+(use-package telephone-line
+  :ensure t
+  :config
+  (telephone-line-mode 1))
+
+;; Helm
+(use-package helm
+  :ensure t
+  :init
+  (setq helm-M-x-fuzzy-match t
+        helm-mode-fuzzy-match t
+        helm-buffers-fuzzy-matching t
+        helm-recentf-fuzzy-match t
+        helm-locate-fuzzy-match t
+        helm-semantic-fuzzy-match t
+        helm-imenu-fuzzy-match t
+        helm-completion-in-region-fuzzy-match t
+        helm-candidate-number-list 150
+        helm-split-window-in-side-p t
+        helm-move-to-line-cycle-in-source t
+        helm-echo-input-in-header-line t
+        helm-autoresize-max-height 0
+        helm-autoresize-min-height 20)
+
+  :config
+  (helm-mode 1))
+
+(use-package helm-ag
+  :ensure t)
+
+;; Vim mode
+(use-package evil
+  :ensure t
+  :config
+  (setq evil-want-integration 1)
+  (evil-mode 1))
 
 ;; Clojure
 (use-package clojure-mode
   :ensure t
   :config
-  (progn
-    (general-define-key
-     :states  'normal
-     :keymaps 'clojure-mode-map
-     :prefix  ","
-     "sc"     'cider-connect
-     "sf"     'cider-find-var)))
+  (general-define-key
+   :states  'normal
+   :keymaps 'clojure-mode-map
+   :prefix  dft-prefix-key
+   "sc"     'cider-connect
+   "sf"     'cider-find-var))
 
 ;; Lisp nav
 (use-package lispy
   :ensure t
   :config
+  (setq lispy-compat '(cider edebug))
+
+  (general-define-key
+   :states  'normal
+   :keymaps '(clojure-mode-map emacs-lisp-mode-map cider-repl-mode-map)
+   :prefix  dft-prefix-key
+   "kdx"    'lispy-kill
+   "kdd"    'lispy-kill-at-point
+   "kdw"    'lispy-kill-word
+   "kds"    'lispy-kill-sentence
+   "kF"     'lispy-follow
+   "km"     'lispy-mark-symbol)
+
   (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
-  (add-hook 'clojure-mode-hook (lambda () (lispy-mode 1)))
-  (add-hook 'cider-repl-mode-hook (lambda () (lispy-mode 1)))
-  (setq lispy-compat '(cider edebug)))
+  (add-hook 'clojure-mode-hook    (lambda () (lispy-mode 1)))
+  (add-hook 'cider-repl-mode-hook (lambda () (lispy-mode 1))))
 
 ;; Lisp Evil nav
 (use-package lispyville
   :ensure t
   :config
-  (add-hook 'lispy-mode-hook #'lispyville-mode))
+  (add-hook 'lispy-mode-hook #'lispyville-mode)
+  (lispyville-set-key-theme
+   '(operators
+     c-w
+     additional-movement
+     slurp/barf-lispy
+     wrap
+     additional
+     additional-insert
+     additional-wrap)))
 
 (use-package cider
   :ensure t
@@ -268,7 +252,7 @@ or the current buffer directory."
   (general-define-key
    :states  'normal
    :keymaps 'cider-repl-mode-map
-   :prefix  ","
+   :prefix  dft-prefix-key
    "sc"     'cider-repl-clear-buffer))
 
 ;; Auto completion
@@ -281,8 +265,56 @@ or the current buffer directory."
 (use-package magit
   :ensure t)
 
-;; Fancy mode line
-(use-package telephone-line
+(use-package ivy
   :ensure t
   :config
-  (telephone-line-mode 1))
+  (ivy-mode 1)
+  (setq magit-completing-read-function 'ivy-completing-read)
+  (setq projectile-completion-system 'ivy))
+
+;; Basic navigation
+(general-define-key
+ :keymaps '(normal visual emacs)
+ :prefix  dft-prefix-key
+
+ "TAB" '(switch-to-prev-buffer :which-key "Buffer previous")
+ "SPC" '(helm-M-x              :which-key "M-x")
+
+ ;; Project
+ "pf"  '(helm-find-files            :which-key "Project find file")
+ "pt"  '(neotree-project-dir-toggle :which-key "Project tree")
+ "pp"  '(projectile-switch-project  :which-key "Project switch")
+ "ps"  '(helm-ag                    :which-key "Project search")
+
+ ;; Files
+ "ff"  '(find-file           :which-key "File find")
+ "fr"  '(recentf             :which-key "File recent")
+
+ ;; Buffers
+ "bs"  '(switch-to-buffer    :which-key "Switch buffer")
+ "bb"  '(helm-mini           :which-key "Helm mini")
+ "bd"  '(kill-current-buffer :whick-key "Kill buffer")
+
+ ;; Window
+ "wl"  '(windmove-right       :which-key "Window move right")
+ "wh"  '(windmove-left        :which-key "Window move left")
+ "wk"  '(windmove-up          :which-key "Window move up")
+ "wj"  '(windmove-down        :which-key "Window move bottom")
+ "w/"  '(split-window-right   :which-key "Window split right")
+ "w-"  '(split-window-below   :which-key "Window split bottom")
+ "wd"  '(delete-window        :which-key "Window kill")
+ "wm"  '(delete-other-windows :which-key "Window maximize")
+
+ ;; Others
+ "at"  '(ansi-term            :which-key "Terminal open"))
+
+;; Global C-g override
+(general-define-key
+ :keymaps 'key-translation-map "ESC" "C-g")
+
+;; Emacs Lisp
+(general-define-key
+ :states   'normal
+ :keymaps  'emacs-lisp-mode-map
+ :prefix   dft-prefix-key
+ "eb"      '(eval-buffer :which-key "Eval buffer"))
