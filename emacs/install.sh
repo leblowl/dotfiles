e15899ad56de140abffe5cf00a095daa0c647756
Lucas Leblow <lucasleblow@mailbox.org>
2021-09-17 21:52:58 -0600

    Add yaml-mode.el to emacs install script

--- emacs/install.sh
+++ emacs/install.sh
@@ -6,3 +6,4 @@ mkdir -p $install_dir
 ln -s $PWD/init.el $base_dir/init.el
 ln -s $PWD/keys.el $install_dir/keys.el
 ln -s $PWD/ob-racket.el $install_dir/ob-racket.el
+ln -s $PWD/yaml-mode.el $install_dir/yaml-mode.el
